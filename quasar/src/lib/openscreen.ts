f1132fa789f1345ee4dd42cd416d04fb219bd9f7
Alain Nguyen <46081985+Aloinn@users.noreply.github.com>
2022-05-07 13:52:05 -0400

    openscreen

--- quasar/src/lib/openscreen.ts
+++ quasar/src/lib/openscreen.ts
@@ -1,13 +1,13 @@
-import { metaQR } from './models';
+import { CreateAssetByProjectIdRequestBody, Openscreen } from '@openscreen/sdk';
 
-const { Openscreen } = require('@openscreen/sdk');
 require('dotenv').config();
-const os = new Openscreen().config({
-  key: process.env.OS_API_KEY,
-  secret: process.env.OS_API_SECRET,
-});
-
 const projectId = '1a877553-f7e3-4bb9-a5bb-0dd0cfa7e28e';
+export const openScreen = new Openscreen()
+  .config({
+    key: process.env.OS_API_KEY,
+    secret: process.env.OS_API_SECRET,
+  })
+  .project(projectId);
 
 /**
  * creates an asset object
@@ -16,8 +16,8 @@ const projectId = '1a877553-f7e3-4bb9-a5bb-0dd0cfa7e28e';
  * Asset object is a physical or digital object that is represented by a QR code.
  * e.g Companies, Products etc..
  */
-export async function createAsset(metaData: metaQR) {
+export async function createAsset(metaData: CreateAssetByProjectIdRequestBody) {
   // Asset creation,
-  const res = await os.project(projectId).assets().create(metaData);
+  const res = await openScreen.assets().create(metaData);
   console.log(res);
 }

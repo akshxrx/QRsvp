7f37e1b4f030cb1e0d9abe5d3c7a64b72c80175b
Alain Nguyen <46081985+Aloinn@users.noreply.github.com>
2022-05-07 13:09:16 -0400

    Fixed dummy bug

--- quasar/src/components/FirebaseTest.vue
+++ quasar/src/components/FirebaseTest.vue
@@ -1,13 +1,25 @@
 <template>
-  <q-btn> </q-btn>
+  <div>
+    <q-btn> </q-btn>
+    {{ data }}
+    {{ t.data }}
+  </div>
 </template>
 
 <script lang="ts">
+import {
+  useFirestoreCollection,
+  useFirestoreDoc,
+} from '@gcto/firebase-hooks/lib';
 import { defineComponent } from '@vue/runtime-core';
 
 export default defineComponent({
   setup() {
-    return {};
+    const { data } = useFirestoreCollection('test');
+    const t = useFirestoreDoc('T', () => 'vfdhmJ7Zu5hK4QB9ro0O');
+
+    // const t = collection.
+    return { data, t };
   },
 });
 </script>

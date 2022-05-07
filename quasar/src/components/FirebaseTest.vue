8062c65102a0c486d4e48dae864c1c8b4e706646
Alain Nguyen <46081985+Aloinn@users.noreply.github.com>
2022-05-07 12:37:41 -0400

    Added firebase integration

---
+++ quasar/src/components/FirebaseTest.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-btn> </q-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
---
+++ quasar/src/components/VueTemplate.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>a</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
